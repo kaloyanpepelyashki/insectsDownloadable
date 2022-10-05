31b10bee382802f3b2b2d383148760caa8b873d3
kaloyanpepelyashki <93147724+kaloyanpepelyashki@users.noreply.github.com>
2022-10-05 09:20:08 +0100

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -24,7 +24,7 @@ self.addEventListener("install", function (event) {
 // If the user request ressource (file, html, image etc) then look for it online
 // If not available online, get file from cache
 self.addEventListener("fetch", function (event) {
-  event.respond(
+  event.respondWith(
     fetch(event.request).catch(() =>
       caches.open(cacheName).then((cache) => cache.match(event.request))
     )
